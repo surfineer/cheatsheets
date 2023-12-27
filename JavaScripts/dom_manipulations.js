4926c2424ad71e7ac0ed3eb5be3926a24b22018b
surfineer <123465523+surfineer@users.noreply.github.com>
2023-12-27 21:49:53 +0800

    JS updates

---
+++ JavaScripts/dom_manipulations.js
@@ -0,0 +1,10 @@
+// addressing HTML elements
+document.querySelector('body').style.backgroundColor = 'purple'
+$('h1').style.backgroundColor = 'pink'; // $('element') is an alias for document.querySelector
+
+// window pop-ups
+window.alert('You are leaving...'); // can only click 'ok'
+window.confirm('Are you sure?'); // Can click 'ok' or 'cancel'
+window.prompt('Enter your name'); // input window will return user's input
+
+
