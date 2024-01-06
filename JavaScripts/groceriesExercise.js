b8701c9c6c9093c1add107dbcc46df168c9a62cf
surfineer <123465523+surfineer@users.noreply.github.com>
2024-01-06 15:45:59 +0800

    init new Javascript exercise

---
+++ JavaScripts/groceriesExercise.js
@@ -0,0 +1,15 @@
+/*
+Write a function groceries() that takes an array of object literals of grocery items. The function should return a string with each item separated by a comma except the last two items should be separated by the word 'and'. Make sure spaces (' ') are inserted where they are appropriate.
+
+*/
+
+
+
+groceries( [{item: 'Carrots'}, {item: 'Hummus'}, {item: 'Pesto'}, {item: 'Rigatoni'}] );
+// returns 'Carrots, Hummus, Pesto and Rigatoni'
+
+groceries( [{item: 'Bread'}, {item: 'Butter'}] );
+// returns 'Bread and Butter'
+
+groceries( [{item: 'Cheese Balls'}] );
+// returns 'Cheese Balls'
